5e54e6d7c15f8b61ca75a7fd1c1cfbeb5697b35b
kpaszkowski <31098746+kpaszkowski@users.noreply.github.com>
2017-10-08 13:23:20 +0200

    Drop , Move , Show

---
+++ Tetris/Block.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Block
+    {
+        public static int[,] area;
+        public static int[] lastLocation = new int[2]; 
+        public static bool droped = false;
+        int[,] I = new int[4, 4] { { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        int[,] O = new int[4, 4] { { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        int[,] J = new int[4, 4] { { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+        int[,] L = new int[4, 4] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
+        int[,] T = new int[4, 4] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        int[,] S = new int[4, 4] { { 0, 1, 1, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        int[,] Z = new int[4, 4] { { 1, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+
+        public Block(int seed)
+        {
+            Random rand = new Random(seed);
+            List<int[,]> shapeList = new List<int[,]>() { I, O, J, L, T, S, Z };
+            area = shapeList[rand.Next(0, 7)];
+            Prepare();
+            
+        }
+        public Block(string id)
+        {
+            List<int[,]> shapeList = new List<int[,]>() { I, O, J, L, T, S, Z };
+            area = shapeList[Int32.Parse(id)];
+            Prepare();
+        }
+        public static void Prepare()
+        {
+            //umiescic na mapie klocek
+            int locationX = Program.generateLocation[1];
+            int locationY = Program.generateLocation[0];
+            lastLocation[1] = locationX;
+            lastLocation[0] = locationY;
+            for (int i = 0; i < area.GetLength(0); i++)
+            {
+                for (int j = 0; j < area.GetLength(1); j++)
+                {
+                    if (area[i,j]==1)
+                    {
+                        Program.map[locationY, locationX] = 1;
+                    }
+                    locationX += 1;
+                }
+                locationY += 1;
+                locationX -= area.GetLength(1);
+
+            }
+        }
+        public void Drop()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0)+lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1)+lastLocation[1]; j++)
+                {
+                    if (area[i-lastLocation[0],j-lastLocation[1]]==1)
+                    {
+                        Program.map[i, j] = 0;
+                        //Program.map[i + 1, j] = 1;
+                    }
+                }
+            }
+            lastLocation[0] += 1;
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        //Program.map[i, j] = 0;
+                        Program.map[i , j] = 1;
+                    }
+                }
+            }
+            //lastLocation[1] = 1;
+        }
+        public void MoveLeft()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        Program.map[i, j] = 0;
+                        //Program.map[i + 1, j] = 1;
+                    }
+                }
+            }
+            lastLocation[1] -= 1;
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        //Program.map[i, j] = 0;
+                        Program.map[i, j] = 1;
+                    }
+                }
+            }
+        }
+        public void MoveRight()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        Program.map[i, j] = 0;
+                        //Program.map[i + 1, j] = 1;
+                    }
+                }
+            }
+            lastLocation[1] += 1;
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        //Program.map[i, j] = 0;
+                        Program.map[i, j] = 1;
+                    }
+                }
+            }
+        }
+        public void MoveDown()
+        {
+            if (!IsSomethingUnder())
+            {
+                Drop();
+            }
+        }
+        public void MoveFastDown()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                if (!IsSomethingUnder())
+                {
+                    Drop();
+                }
+            }
+        }
+        public bool IsSomethingUnder()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        if (Program.map[i + 1, j] == -1 || Program.map[i + 1, j] == 2 )
+                        {
+                            //zmienianie ustawionego bloku
+                            for (int x = lastLocation[0]; x < area.GetLength(0) + lastLocation[0]; x++)
+                            {
+                                for (int y = lastLocation[1]; y < area.GetLength(1) + lastLocation[1]; y++)
+                                {
+                                    if (area[x - lastLocation[0], y - lastLocation[1]] == 1)
+                                    {
+                                        //Program.map[i, j] = 0;
+                                        Program.map[x, y] = 2;
+                                    }
+                                }
+                            }
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool IsSomethingRight()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        if (Program.map[i , j+1] == -1 || Program.map[i , j+1] == 2 )
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public bool IsSomethingLeft()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        if (Program.map[i, j - 1] == -1 || Program.map[i, j - 1] == 2)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        public void RotateRight()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        Program.map[i, j] = 0;
+                        //Program.map[i + 1, j] = 1;
+                    }
+                }
+            }
+        }
+        //update?
+        public void UpdateBlock()
+        {
+            for (int i = lastLocation[0]; i < area.GetLength(0) + lastLocation[0]; i++)
+            {
+                for (int j = lastLocation[1]; j < area.GetLength(1) + lastLocation[1]; j++)
+                {
+                    if (area[i - lastLocation[0], j - lastLocation[1]] == 1)
+                    {
+                        //update
+                        Console.SetCursorPosition(j, i);
+                        Console.Write("*");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/*
+       #region Shape
+       int[,] I_1 = new int[4, 4] { { 1, 1, 1, 1 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] I_2 = new int[4, 4] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 } };
+       int[,] O_1 = new int[4, 4] { { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] L_1 = new int[4, 4] { { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] L_2 = new int[4, 4] { { 0, 0, 0, 0 }, { 1, 1, 1, 0 }, { 1, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] L_3 = new int[4, 4] { { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] L_4 = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 1, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 } };
+       int[,] J_1 = new int[4, 4] { { 0, 1, 0, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] J_2 = new int[4, 4] { { 0, 0, 0, 0 }, { 1, 0, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 } };
+       int[,] J_3 = new int[4, 4] { { 1, 1, 0, 0 }, { 1, 0, 0, 0 }, { 1, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] J_4 = new int[4, 4] { { 0, 0, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 0 } };
+       int[,] T_1 = new int[4, 4] { { 0, 1, 0, 0 }, { 1, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] T_2 = new int[4, 4] { { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 1, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] T_3 = new int[4, 4] { { 0, 0, 0, 0 }, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] T_4 = new int[4, 4] { { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] S_1 = new int[4, 4] { { 0, 1, 1, 0 }, { 1, 1, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] S_2 = new int[4, 4] { { 1, 0, 0, 0 }, { 1, 1, 0, 0 }, { 0, 1, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] Z_1 = new int[4, 4] { { 1, 1, 0, 0 }, { 0, 1, 1, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       int[,] Z_2 = new int[4, 4] { { 0, 1, 0, 0 }, { 1, 1, 0, 0 }, { 1, 0, 0, 0 }, { 0, 0, 0, 0 } };
+       #endregion
+       */
--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -3,13 +3,151 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Tetris
 {
     class Program
     {
+        public static int N=25, M=12;
+        public static int[] generateLocation = { 0 , 4 };
+        public static string square = "■";
+        public static Stopwatch timeStart = new Stopwatch();
+        public static int timeToEnd = 300;
+        public static int[,] map = new int[N, M];
+        public static ConsoleKeyInfo key;
+        public static bool keyPressed = false;
+        public static bool action = false;
         static void Main(string[] args)
         {
+            PrepareMap();
+            StartGame();
+            Console.ReadLine();
+        }
+        public static void PrepareMap()
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (i==N-1 || j==0 || j==M-1)
+                    {
+                        map[i, j] = -1;
+                        //Console.Write("* ");
+                    }
+                    else
+                    {
+                        //Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void StartGame()
+        {
+            UpdateMap();
+            Block block = GenerateBlock();
+            while (true)
+            {
+                timeStart.Start();
+                if ((int)timeStart.ElapsedMilliseconds>timeToEnd)//co 0,5 sek generuje nowy klocek
+                {
+                    if (!block.IsSomethingUnder())
+                    {
+                        Thread.Sleep(timeToEnd);
+                        block.Drop();
+                        action = true;
+                        block.UpdateBlock();
+                        timeStart.Reset();
+                    }
+                    else
+                    {
+                        block = GenerateBlock();
+                    }
+                }
+                ClickEvent(block);
+                if (action)
+                {
+                    UpdateMap();
+                    action = false;
+                }
+            }
+
+        }
+        public static void UpdateMap()
+        {
+            Console.Clear();
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i,j]==-1)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if (map[i,j]==1)
+                    {
+                        Console.Write(square+" ");
+                    }
+                    else if (map[i,j]==2)
+                    {
+                        Console.Write(square+" ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        public static Block GenerateBlock()
+        {
+            Random rand = new Random();
+            Block block = new Block(rand.Next());
+            return block;
+        }
+        public static void ClickEvent(Block block)
+        {
+            if (Console.KeyAvailable)
+            {
+                key = Console.ReadKey();
+                keyPressed = true;
+                action = true;
+            }
+            else
+            {
+                keyPressed = false;
+            }
+            if (key.Key==ConsoleKey.RightArrow && keyPressed && !block.IsSomethingRight())
+            {
+                block.MoveRight();
+            }
+            if (key.Key == ConsoleKey.LeftArrow && keyPressed && !block.IsSomethingLeft())
+            {
+                block.MoveLeft();
+            }
+            if (key.Key == ConsoleKey.P && keyPressed)
+            {
+                Console.ReadKey();
+            }
+            if (key.Key == ConsoleKey.DownArrow && keyPressed)
+            {
+                block.MoveDown();
+            }
+            if (key.Key == ConsoleKey.Spacebar && keyPressed)
+            {
+                block.MoveFastDown();
+            }
+            if (key.Key == ConsoleKey.UpArrow && keyPressed)
+            {
+                block.RotateRight();
+            }
+            if (key.Key == ConsoleKey.D && keyPressed)
+            {
+                block.RotateRight();
+            }
         }
     }
 }
